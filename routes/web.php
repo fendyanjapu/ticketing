cc59512854373d4526e7f84c64e3fe177d7796da
Diskominfobidegov <Diskominfobidegov@gmail.com>
2025-03-06 12:24:39 +0800

    Halaman Login

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\TicketController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('dashboard', function () {
